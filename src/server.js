11bb7e2d70bb46205a529d880a6e2800b8aacdf5
Nico De Witte <nico.dewitte@outlook.com>
2020-11-17 08:41:24 +0100

    WIP - posting to backend

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
-const mqtt = require('mqtt')
+const mqtt = require('mqtt');
+const axios = require('axios');
 
 console.log("Starting IP report listener");
 
 let client  = mqtt.connect('mqtt://10.0.0.200');     // ENV VAR !!!!
+
+const api = axios.create({
+  baseURL: 'http://localhost:8081',         // ENV !!!!
+});
  
 client.on('connect', function () {
   client.subscribe('test/dhcp/detect', function (err) {   // ENV VAR !!!
@@ -14,7 +19,20 @@ client.on('message', function (topic, message) {
   if (topic === 'test/dhcp/detect') {
     let data = JSON.parse(message.toString());
     console.log(data);
+
+    // {
+    //   "time": "Tue Nov 10 15:02:34 UTC 2020",
+    //   "mac": "cc:9f:7a:ab:ab:f2",
+    //   "ip": "10.0.0.2",
+    //   "hostname": "unknown"
+    // }
+    
+
+
     // POST HERE TO BACKEND
+    // axios.post('/devices', {
+      
+    // })
   }
 });
 
